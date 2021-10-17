e3503cd64f93efe93c3aa066a610061fc9915485
francisco tena <ftenaf@yahoo.es>
2021-10-17 20:50:57 +0100

    user details service bean

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserRepositoryUserDetailsService.java
@@ -17,6 +17,11 @@ public class UserRepositoryUserDetailsService implements UserDetailsService {
         this.users = users;
     }
 
+    @Bean
+    UserDetailsService userDetailsService(UserRepository users) {
+        return new UserRepositoryUserDetailsService(users);
+    }
+    
     @Override
     public UserDetails loadUserByUsername(String username) {
         return this.users.findByUsername(username)
