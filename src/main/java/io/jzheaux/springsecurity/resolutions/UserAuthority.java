74cfd8ff3a516572639442c21cacdc4196964399
francisco tena <ftenaf@yahoo.es>
2021-10-17 19:59:52 +0100

    pre authorities

--- src/main/java/io/jzheaux/springsecurity/resolutions/UserAuthority.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/UserAuthority.java
@@ -1,4 +1,31 @@
 package io.jzheaux.springsecurity.resolutions;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@Entity(name="authorities")
 public class UserAuthority {
+    @Id
+    UUID id;
+
+    @Column
+    String authority;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
 }
